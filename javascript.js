418cf027d9da1989834bbb3015271788599a3d08
azanra <112457939+azanra@users.noreply.github.com>
2024-04-09 12:11:18 +0700

    Add score counter and decide the winner

--- javascript.js
+++ javascript.js
@@ -3,16 +3,26 @@ function getComputerChoice() {
     return arrayChoice[(Math.floor(Math.random() * arrayChoice.length))];
 }
 
+function playerWin(){
+    return playerScore++;
+}
+
+function playerLose(){
+    return computerScore++;
+}
+
 function playRound(playerSelection, computerSelection){
     switch(playerSelection) {
         case "paper":
             if(computerSelection === "paper"){
                 return("it's a tie, you both draw paper!");
             }
-            else if(computerSelection === "rock"){
+           else if(computerSelection === "rock"){
+                playerWin();
                 return("you win!, paper beats rock!");
             }
             else{
+                playerLose();
                 return("you lose!, sciccor beats paper!");
             }
             break;
@@ -21,21 +31,37 @@ function playRound(playerSelection, computerSelection){
                 return("it's a tie, you both draw rock!");
             }
             else if(computerSelection === "sciccor"){
+                playerWin();
                 return("you win!, rock beats sciccor!");
             }
             else{
+                playerLose();
                 return("you lose!, paper beats rock!");
             }
         case "sciccor":
-                if(computerSelection === "sciccor"){
-                    return("it's a tie, you both draw sciccor");
-                }
-                else if(computerSelection === "paper"){
-                    return("you win!, sciccor beats paper!");
-                }
-                else{
-                    return("you lose!, rock beats sciccor!");
-                }
+            if(computerSelection === "sciccor"){
+                return("it's a tie, you both draw sciccor");
+            }
+            else if(computerSelection === "paper"){
+                playerWin();
+                return("you win!, sciccor beats paper!");
+            }
+            else{
+                playerLose();
+                return("you lose!, rock beats sciccor!");
+            }
+    }
+}
+
+function checkWinner(){
+    if(playerScore > computerScore){
+        console.log(`you win with a score of ${playerScore} agaisnt ${computerScore}`);
+    }
+    else if(playerScore === computerScore){
+        console.log(`it's a tie with both score of ${playerScore}`);
+    }
+    else{
+        console.log(`you lose with score of ${playerScore} against ${computerScore}`);
     }
 }
 
@@ -46,8 +72,11 @@ function playGame(){
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }   
+    checkWinner();
 }
 
+let playerScore = 0;
+let computerScore = 0;
 playGame();
 
 
